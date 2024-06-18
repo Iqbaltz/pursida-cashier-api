de701490b76e89852ac0a3fd6c42a11f2cea7d06
Iqbal <mhdiqbaltan@gmail.com>
2024-06-18 13:59:19 +0700

    fix-wrong-entityy

--- app/Http/Controllers/CashierTransactionController.php
+++ app/Http/Controllers/CashierTransactionController.php
@@ -319,7 +319,7 @@ class CashierTransactionController extends Controller
             'kasir' => $transaction->cashier_name,
             'pelanggan' => $transaction->customer_name,
             'alamat' => $transaction->customer ? $transaction->customer->address : '-',
-            'no_telp' => $transaction->customer ? $transaction->customer->phone : '-',
+            'no_telp' => $transaction->customer ? $transaction->customer->phone_number : '-',
             'items' => $transaction->transaction_items->map(fn ($x) => [
                 'name' => $x->barang_name,
                 'qty' => $x->qty,
